1eb44cb2c781dbfd981b785904d4b09ecc214a69
buckaroo69 <diogobento@ua.pt>
2021-11-05 15:58:27 +0000

    datatable generation method

--- assignment1/datagen.sh
+++ assignment1/datagen.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
-rm datatable.ssv #reset file we're appending to
+rm datatable.csv #reset file we're appending to
 for file in *.tsv.gz
 do
-rm blockdumb*
+rm blockdump*
 rm mergedindex*
 rm masterindex*
+time1=$(python3 indexer.py --source $file)
+tempfilecount=$(find . -maxdepth 1 -name "blockdump*" |wc -l)
+time2=$(python3 merger.py)
+indextime=$(echo "$time1 + $time2" | bc)
+boottime=$(python3 loader.py --timer-only)
+vocabsize=$(wc -l < masterindex.ssv)
+indexsize=$(stat -c%s masterindex.ssv)
+for f in mergedindex*
+do
+add=$(stat -c%s $f)
+indexsize=$(expr $add + $indexsize)
+done
+echo "$file,$indextime,$indexsize,$vocabsize,$tempfilecount,$boottime" >>datatable.csv
 
 done
