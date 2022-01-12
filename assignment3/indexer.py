797afebb6e9e240c5019903afd6bf362689b36cb
buckaroo69 <diogobento@ua.pt>
2022-01-12 12:35:21 +0000

    phase one mostly planned out

--- assignment3/indexer.py
+++ assignment3/indexer.py
@@ -12,7 +12,7 @@ from time import perf_counter
 csv.field_size_limit(sys.maxsize)
 
 
-def process_file(file,delimiter, relevant_columns, min_length, stopwords, stemmer,break_size,dumpprefix,metadatafilename):
+def process_file(file,delimiter, relevant_columns, min_length, stopwords, stemmer,break_size,dumpprefix,metadatafilename,storePositions):
     #DESCRIPTION: LOADS FILE, GETS CSV ARRAY AND A HEADER DICTIONARY, CREATES AND DUMPS TOKEN SEQUENCES
     reader = csv.reader(file,delimiter=delimiter)
     #BUILD HEADER
@@ -30,6 +30,7 @@ def process_file(file,delimiter, relevant_columns, min_length, stopwords, stemme
             continue
         seq_id+=1
         doc_len = 0
+        word_offset = 0
         for column_name in relevant_columns:
             text = item[headerdict[column_name]]
             doc_len+=len(text)
@@ -42,23 +43,25 @@ def process_file(file,delimiter, relevant_columns, min_length, stopwords, stemme
                 if word in stopwords or re.match(r".*(.)\1{3,}.*", word):
                     #does not add stopwords or terms with more than 3 equal consecutive symbols
                    continue
-                current_items.append(( stemmer.stem(word) ,seq_id ))
+                if storePositions:
+                    current_items.append(( stemmer.stem(word) ,word_offset))
+                else:
+                    current_items.append(( stemmer.stem(word) ,seq_id))
+                word_offset+=1
             if sys.getsizeof(current_items) > 1024*1024*break_size:
-                dump_into_file(f"{dumpprefix}{current_block}.ssv",current_items)
+                dump_into_file(f"{dumpprefix}{current_block}.ssv",current_items,storePositions)
                 del current_items
                 gc.collect() #clear memory
                 current_items = []
                 current_block+=1
         docsinfo.append((seq_id,item[headerdict['review_id']],doc_len))
     if current_items:
-        dump_into_file(f"{dumpprefix}{current_block}.ssv",current_items)
+        dump_into_file(f"{dumpprefix}{current_block}.ssv",current_items,storePositions)
 
     dump_metadata(seq_id+1,docsinfo,metadatafilename)
 
 
 def dump_metadata(doccount,documentinfo,outputfile):
-    #NEW IN ASSIGNMENT 2
-    # dumps average doc length, total doc count, and maps sequential IDs to actual IDs to document length
     avglen = sum([x[2] for x in documentinfo])/doccount
     metadatafile = open(outputfile,"w")
     metadatafile.write(f"{doccount} {avglen}\n")
@@ -66,20 +69,29 @@ def dump_metadata(doccount,documentinfo,outputfile):
         metadatafile.write(f"{x[0]} {x[1]} {x[2]}\n")
     metadatafile.close()
 
-def dump_into_file(outputfile,current_items):
+def dump_into_file(outputfile,current_items,storePositions):
     f = open(outputfile,"w")
     current_items = sort_terms(current_items)
-    alternate_structure_items = restructure_as_map(current_items)
+    alternate_structure_items = restructure_as_map(current_items,storePositions)
     for x,y in alternate_structure_items.items():
         f.write(x+" ")
-        f.write(" ".join([f"{doc}:{count}" for doc,count in y])+"\n")
+        if storePositions:
+            f.write(" ".join([f"{doc}:{count}:{','.join(positions)}" for doc,count,positions in y])+"\n")
+        else:
+            f.write(" ".join([f"{doc}:{count}" for doc,count in y])+"\n")
     f.close()
 
 def sort_terms(array): #DESCRIPTION: SORTS TOKEN SEQUENCE
     return sorted(array,key=lambda x: (x[0],x[1]))
 
 
-def restructure_as_map(ordered): #DESCRIPTION: MAPS ORDERED TERMS TO TERMS->DOC_SET
+def restructure_as_map(ordered,storePositions): #DESCRIPTION: MAPS ORDERED TERMS TO TERMS->DOC_SET
+    if storePositions:
+        return restructure_as_map_positions(ordered)
+    else:
+        return restructure_as_map_no_positions(ordered)
+
+def restructure_as_map_no_positions(ordered): #DESCRIPTION: MAPS ORDERED TERMS TO TERMS->DOC_SET
     current = ""
     postingslist= []
     index = {}
@@ -88,6 +100,38 @@ def restructure_as_map(ordered): #DESCRIPTION: MAPS ORDERED TERMS TO TERMS->DOC_
     when term changes save progreess
     """
     for term,id in ordered:
+        if current!=term:
+            if current!="":
+                postingsMap = Counter(postingslist)#automatically maps how many times each document has a certain word
+                postingslist = sorted(set(postingslist))
+                gaps = [(postingslist[0],postingsMap[postingslist[0]])] #each gap has an associated doc-word number
+                for i in range(len(postingslist))[1:]:
+                    gaps+= [(postingslist[i]-postingslist[i-1],postingsMap[postingslist[i]])]
+                index[current]=gaps #save
+            #reset
+            current = term
+            postingslist= []
+        #update
+        postingslist.append(id)
+    postingsMap = Counter(postingslist)#automatically maps how many times each document has a certain word
+    postingslist = sorted(set(postingslist))
+    gaps = [(postingslist[0],postingsMap[postingslist[0]])] #each gap has an associated doc-word number
+    for i in range(len(postingslist))[1:]:
+        gaps+= [(postingslist[i]-postingslist[i-1],postingsMap[postingslist[i]])]
+    index[current]=gaps #save
+
+    return index
+
+def restructure_as_map_positions(ordered):
+    #TODO: THIS WHOLE ASS THING
+    current = ""
+    postingslist= []
+    index = {}
+    """"
+    if term is same add to current ID set, add +1
+    when term changes save progreess
+    """
+    for term,id,positions in ordered:
         if current!=term:
             if current!="":
                 postingsMap = Counter(postingslist)#automatically maps how many times each document has a certain word
@@ -137,6 +181,11 @@ if __name__=="__main__":
     #input file
     parser.add_argument("--source",help="Source file, please provide gzip compatible files", default="amazon_reviews_us_Digital_Video_Games_v1_00.tsv.gz")
 
+    #positions
+    parser.add_argument('--positions', dest='pos', action='store_true')
+    parser.add_argument('--no-positions', dest='pos', action='store_false')
+    parser.set_defaults(positions=True)
+
     args = parser.parse_args()
 
 
@@ -157,7 +206,7 @@ if __name__=="__main__":
         stemmer = UselessStemmer()
 
     timedelta =perf_counter()
-    process_file(f,"\t",relevant_columns, args.lenfilter,stopwords,stemmer,args.stopsize,args.prefix,args.metadata)
+    process_file(f,"\t",relevant_columns, args.lenfilter,stopwords,stemmer,args.stopsize,args.prefix,args.metadata,args.pos)
     timedelta = perf_counter()-timedelta
     print(timedelta)
     f.close()
